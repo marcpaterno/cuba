274750a77b715bc3ff2de13f9e006afc0098dd8e
Marc Paterno <paterno@fnal.gov>
2020-09-30 15:27:33 -0500

    Get library name right on macOS

--- makesharedlib.sh
+++ makesharedlib.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 # Author: Johannes Buchner (C) 2015
+# Modification: Marc Paterno (C) 2020
 # For creating a shared library (libcuba.so).
+
+os_string=$(uname)
+if [[ "${os_string}" == "Darwin" ]]; then
+  echo "I am on macOS"
+elif [[ "${os_string}" == "Linux" ]]; then
+  echo "I am on Linux"
+else
+  echo "Neither Darwin (macOS) nor Linux detected; aborting installation."
+  exit 1
+fi
 
 sed 's/CFLAGS = -O3 -fomit-frame-pointer/CFLAGS = -O3 -fPIC -fomit-frame-pointer/g' --in-place makefile
 echo "rebuilding libcuba.a archive"
 make -B libcuba.a
 echo "unpacking libcuba.a"
 FILES=$(ar xv libcuba.a | sed 's/x - //g' | fgrep -v "__.SYMDEF")
-echo "making libcuba.so"
-echo gcc -shared -Wall $FILES -lm -o libcuba.so
-gcc -shared -Wall $FILES -lm -o libcuba.so
+echo "Making shared library"
+
+LIBNAME=libcuba.so
+if [[ "${os_string}" == "Darwin" ]]; then
+  LIBNAME=libcuba.dylib
+fi
+
+gcc -shared -Wall $FILES -lm -o ${LIBNAME}
 rm $FILES
 
